53a3a86fa9aeb59e8ea160055da4a6683cf85d02
f1lander <filanderuclez@gmail.com>
2015-11-26 09:32:32 -0600

    cambios en el robo.html

--- js/CommandsDefinition.js
+++ js/CommandsDefinition.js
@@ -7,30 +7,31 @@
 //ACHUBARA -> SALTAR
 
 var principalCommands = [
-{idP: 'md', id:'md',com: 'MOVERDERECHA'},
-{idP: 'ma', id:'ma',com: 'MOVERABAJO'},
-{idP: 'mar', id:'mar',com: 'MOVERARRIBA'},
-{idP: 'mi', id:'mi',com: 'MOVERIZQUIERDA'},
+	{idP: 'md', id:'md',com: 'MOVERDERECHA'},
+	{idP: 'ma', id:'ma',com: 'MOVERABAJO'},
+	{idP: 'mar', id:'mar',com: 'MOVERARRIBA'},
+	{idP: 'mi', id:'mi',com: 'MOVERIZQUIERDA'},
 
-{idP: 'sd', id:'sd',com: 'SALTARDERECHA'},
-{idP: 'sa', id:'sa',com: 'SALTARABAJO'},
-{idP: 'sar', id:'sar',com: 'SALTARARRIBA'},
-{idP: 'si', id:'si',com: 'SALTARIZQUIERDA'},
+	{idP: 'sd', id:'sd',com: 'SALTARDERECHA'},
+	{idP: 'sa', id:'sa',com: 'SALTARABAJO'},
+	{idP: 'sar', id:'sar',com: 'SALTARARRIBA'},
+	{idP: 'si', id:'si',com: 'SALTARIZQUIERDA'},
 
-{idP: 're', id:'re',com: 'REPETIR'},
-{idP: 'var', id:'var',com: 'VAR'}
+	{idP: 're', id:'re',com: 'REPETIR'},
+	{idP: 'var', id:'var',com: 'VAR'}
 ];
 
 var garifunaCommands = [
-{idP:'md', id:'ou', com:'OUWEREN'},
-{idP:'ma', id:'in', com:'IÑUN'},
-{idP:'mar', id:'un', com:'UNABU'},
-{idP:'mi', id:'bo', com:'BOUNA'},
 
-{idP:'sd', id:'acou', com:'ACHUBARAOUWEREN'},
-{idP:'sa', id:'acin', com:'ACHUBARAIÑUN'},
-{idP:'sar', id:'acun', com:'ACHUBARAUNABU'},
-{idP:'si', id:'acbo', com:'ACHUBARABOUNA'}
+	{idP:'md', id:'ou', com:'OUWEREN'},
+	{idP:'mi', id:'in', com:'IÑUN'},
+	{idP:'mar', id:'un', com:'UNABU'},
+	{idP:'ma', id:'bo', com:'BOUNA'},
+
+	{idP:'sd', id:'acou', com:'ACHUBARAOUWEREN'},
+	{idP:'si', id:'acin', com:'ACHUBARAIÑUN'},
+	{idP:'sar', id:'acun', com:'ACHUBARAUNABU'},
+	{idP:'sa', id:'acbo', com:'ACHUBARABOUNA'}
 
 ];
 
